609c24429c753f10eac8a6d2277322c15bfabb48
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2025-09-01 06:28:59 +0500

    database

--- app/bot.py
+++ app/bot.py
@@ -3,12 +3,17 @@ from telegram.ext import (
     CommandHandler,
 )
 from .config import config
+from .database import engine , Base
 from . import handlers
+
+Base.metadata.create_all(engine)
 
 def run_bot() -> None:
     updater = Updater(config.BOT_TOKEN)
     
     dispatcher = updater.dispatcher
+    
+    
     
     dispatcher.add_handler(CommandHandler("start" , handlers.start))
     
